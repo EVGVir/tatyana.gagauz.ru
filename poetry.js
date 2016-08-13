30b6a620bbbe4f4a2cc613c256cc0a0b74d5f923
Evgeny Gagauz <evgenij.gagauz@gmail.com>
2016-08-13 23:27:30 +0000

    A 'main page' with a photo is introduced.

--- poetry.js
+++ poetry.js
@@ -1,7 +1,6 @@
-function loadPoem() {
+// Loads a poem into the 'content' container.
+function loadPoem(poem) {
     var xmlhttp = new XMLHttpRequest();
-
-    var poem = window.location.hash.replace(/^#/, "");
 
     xmlhttp.onreadystatechange = function() {
         if (xmlhttp.readyState == 4) {
@@ -16,6 +15,22 @@ function loadPoem() {
 
     xmlhttp.open("GET", "poems/" + poem + ".html", true);
     xmlhttp.send();
+}
+
+
+// Loads a content into the 'content' container:
+// - a main page (image) is loaded if there is no 'target';
+// - a poem is loaded if a target is specified.
+function loadContent() {
+    var poem = window.location.hash.replace(/^#/, "");
+
+    if (poem) {
+        loadPoem(poem);
+    } else {
+        document.getElementById("content").innerHTML =
+            '<img src="tatyana-gagauz.jpg" style="max-width:100%; width:100%"></img>';
+        // `width` property is set to overcome a bug in the FireFox.
+    }
 
     return true;
 }
